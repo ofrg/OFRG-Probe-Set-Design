74ee6266929e95312087adb612aed765caf1b02d
Paul Ruegger <paulruegger@yahoo.com>
2011-09-20 11:47:23 -0700

    copyright notices added

--- db_column_distance_qsub.pl
+++ db_column_distance_qsub.pl
@@ -23,6 +23,22 @@
 # perl column_distance_run.pl Bacteria.Bacteroidetes.Bacteroidia.Bacteroidales.Prevotellaceae__Prevotella.nodupe.fa Prev 10
 #
 # This command would create 10 jobs from the .fa file, using shell scripts named Prev.1, Prev.2, etc.
+#
+#
+# Copyright (C) 2011 Paul Ruegger
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use Cwd;
